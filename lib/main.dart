36364692b780aea84052b6211879d561d9c75038
[Idowu Grace] <[idowugracea@gmail.com]>
2022-01-08 16:55:51 +0100

    DateTime now added

--- lib/main.dart
+++ lib/main.dart
@@ -12,44 +12,43 @@ void main() {
       home: Scaffold(
         appBar: AppBar(
             title: Column(
+          children: [
+            SafeArea(
+              child: Row(
+                children: [],
+              ),
+            ),
+            Row(
               children: [
-                SafeArea(
-                  child: Row(
-                    children: [
-                      
-                    ],
+                Text(
+                  'WhatsApp',
+                  style: TextStyle(
+                    fontSize: 14.0,
+                  ),
                 ),
+                SizedBox(width: double.infinity / 2),
+                Icon(Icons.search, size: 12.0),
+                SizedBox(width: 10.0),
+                Icon(
+                  FontAwesomeIcons.ellipsisV,
+                  size: 12.0,
+                  semanticLabel: 'ellipsis',
                 ),
-                Row(
-                  children: [
-                        Text(
-                          'WhatsApp',
-                          style: TextStyle(
-                            fontSize: 14.0,
-                          ),
-                        ),
-                        SizedBox(width: double.infinity / 2),
-                        Icon(Icons.search, size: 12.0),
-                        SizedBox(width: 10.0),
-                        Icon(
-                          FontAwesomeIcons.ellipsisV, size: 12.0, semanticLabel: 'ellipsis',
-                        ),     
-                  ],
-                    ),
               ],
-            )),
+            ),
+          ],
+        )),
       ),
     ),
   );
 }
 
-
 class DateWidget extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    DateTime now = new DateTime.now();
+    return Text(
+      '${now.hour}:${now.minute}',
     );
   }
 }
