2c11c5407646a01427c3d1db2223c083a741b2fc
[Idowu Grace] <[idowugracea@gmail.com]>
2022-01-08 16:44:35 +0100

    semanticLabel added to FAIcons

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xFF252526),
+        accentColor: Color(0xFF4EC9B0),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+            title: Column(
+              children: [
+                SafeArea(
+                  child: Row(
+                    children: [
+                      
+                    ],
+                ),
+                ),
+                Row(
+                  children: [
+                        Text(
+                          'WhatsApp',
+                          style: TextStyle(
+                            fontSize: 14.0,
+                          ),
+                        ),
+                        SizedBox(width: double.infinity / 2),
+                        Icon(Icons.search, size: 12.0),
+                        SizedBox(width: 10.0),
+                        Icon(
+                          FontAwesomeIcons.ellipsisV, size: 12.0, semanticLabel: 'ellipsis',
+                        ),     
+                  ],
+                    ),
+              ],
+            )),
+      ),
+    ),
+  );
+}
+
+
+class DateWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
 }
